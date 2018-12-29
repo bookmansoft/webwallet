0f7832fd0a7408e98e5652eab5dd33858bbc5861
pumi365 <pumi365>
2018-12-29 10:35:21 +0800

    ...

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -33,7 +33,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
-    }
+    },
+    disableHostCheck: true
   },
   plugins: [
     new webpack.DefinePlugin({
