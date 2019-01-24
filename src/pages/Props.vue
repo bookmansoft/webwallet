df53a478caa957669d9be71862796fc4322ad283
pumi365 <pumi365>
2019-01-24 18:48:14 +0800

    道具列表改成宫格模式

--- src/pages/Props.vue
+++ src/pages/Props.vue
@@ -4,9 +4,11 @@
       <!--
       <panel header="道具列表" :list="propList" type="5" @on-img-error="onImgError"></panel>
       -->
+      <!--
       <scroller v-show="isShow" v-model="scrollerStatus" height="-46" lock-x scrollbar-y ref="scroller" :bounce="isbounce"
               :use-pullup="showUp" :pullup-config="upobj" @on-pullup-loading="selPullUp">
       <div>
+
         <group :title="groupTitle">
           <div v-for="(item, index) in propList" :key="index" class="propItem">
             <flexbox @click.native="gotoPropDetail(item, index)">
@@ -24,16 +26,29 @@
             </flexbox>
           </div>
         </group>
+        
         <div style="padding: 10px 80px 20px 80px "><divider v-if="isBottom">到底了</divider></div>
+
       </div>
       </scroller>
       <loading v-model="showLoading" :text="textLoading"></loading>
+      -->
+      <group :title="groupTitle">
+        <grid :cols="4" :show-lr-borders="false" :show-vertical-dividers="false">
+            <grid-item v-for="(item, index) in propList" :key="index">
+              <div class="grid-center" @click="gotoPropDetail(item, index)">
+                <img :src="item.result.icon" class="img-prop-list" />
+                <p class="grid-prop-title"><span>{{item.result.props_name}}</span></p>
+              </div>
+            </grid-item>
+        </grid>
+      </group>
   </div>
 </template>
 
 <script>
 
-import {Scroller, Group, XHeader, Panel, Flexbox, FlexboxItem, Loading, Divider} from 'vux'
+import {Scroller, Group, XHeader, Panel, Flexbox, FlexboxItem, Loading, Divider, Grid, GridItem} from 'vux'
 import { setTimeout } from 'timers';
 
 export default {
@@ -42,7 +57,7 @@ export default {
     
   },
   components: {
-    Scroller, Group, XHeader, Panel, Flexbox, FlexboxItem, Loading, Divider
+    Scroller, Group, XHeader, Panel, Flexbox, FlexboxItem, Loading, Divider, Grid, GridItem
   },
   activated () {
       this.$refs.scroller.reset()
@@ -125,6 +140,7 @@ export default {
         that.axios.post(this.GLOBAL.apiUrl, data).then(res => {
             if(res.data.errcode=='success') {
               //console.log('props.length', res.data.props.length)  
+              that.PageIndex++
               if(res.data.props.length >0 ) {
                   res.data.props.sort(function(a, b){
                       return  b.time - a.time;
@@ -141,9 +157,9 @@ export default {
                 that.isBottom = true
               } else {
                 that.scrollerStatus.pullupStatus = 'default' 
+                this.getProps(that.PageIndex)
               }
             }
-            that.PageIndex++
             //that.scrollerReset()
             that.showLoading = false
         });
@@ -180,9 +196,11 @@ export default {
   },
   mounted() {
     this.isShow = true
+    /*
     this.$nextTick(() => {
         this.$refs.scroller.reset()
     })
+    */
     this.getPropCount()
   }
 }
@@ -190,15 +208,30 @@ export default {
 
 <style lang="less" scoped>
 
+.grid-center {
+  display: block;
+  text-align: center;
+  /*color: #666;*/
+  background-color:#FAFAFA;
+}
+
+.grid-prop-title {
+  color: #999;
+  overflow: hidden;
+  text-overflow:ellipsis;
+  white-space: nowrap;
+  width: 85px;
+}
+
 .propItem {
-  background-color: white;
+  background-color: #FAFAFA;
   margin-top: 0.6rem;
   padding: 0.5rem;
 }
 .img-prop-list {
-    width: 4.2rem;
-    height: 4.2rem;
-    border-radius: 12%;
+  width: 4.2rem;
+  height: 4.2rem;
+  border-radius: 12%;
 }
 
 .xs-plugin-pulldown-container {
