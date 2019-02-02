5ec8adfd0da6ef58daf18d70b666e6da8c8a4815
pumi365 <pumi365@qq.com>
2019-02-03 01:44:58 +0800

    抢红包

--- src/pages/RedPack.vue
+++ src/pages/RedPack.vue
@@ -9,7 +9,7 @@
     </group>
     <group label-width="3.5em" label-margin-right="2em" label-align="right">
         <div style="padding:15px;">
-        <x-button type="primary" @click.native="sendRedPack" v-if="mpOpenid!=null">发一个红包试试</x-button>
+        <x-button type="primary" @click.native="sendRedPack" v-if="wxopenid!=null">发一个红包试试</x-button>
         </div>
     </group>
   </div>
@@ -26,7 +26,8 @@ export default {
       version: '领一个红包',
       headerTitle: '抢红包',
       code: null,
-      mpOpenid: null,
+      wxopenid: null,
+      uid: 0,
       retMsg: null
     }
   },
@@ -35,7 +36,7 @@ export default {
         this.$router.go(-1);
       },
       sendRedPack() {
-          let data = {func:'SendRecPack', control: 'wechat', openid: this.mpOpenid, oemInfo: this.GLOBAL.oemInfo}
+          let data = {func:'SendRecPack', control: 'wechat', openid: this.wxopenid, oemInfo: this.GLOBAL.oemInfo}
           this.axios.post(this.GLOBAL.apiUrl, data).then(res => {
               console.log(res.data)
               if(res.data.errcode=='success') {
@@ -51,22 +52,33 @@ export default {
           window.location.href = url
       },
       getOpenid() {
-          let data = {func:'GetMapOpenId', control: 'wechat', code: this.code, oemInfo: this.GLOBAL.oemInfo}
+          console.log('getOpenid')
+          let data = {func:'GetMapOpenId', control: 'wechat', code: this.code, openid: this.GLOBAL.openid, oemInfo: this.GLOBAL.oemInfo}
           this.axios.post(this.GLOBAL.apiUrl, data).then(res => {
               console.log(res.data)
               if(res.data.errcode=='success') {
-                  this.mpOpenid = res.data.ret.openid
+                  this.wxopenid = res.data.userProfile.wxopenid
+                  this.uid = res.data.userProfile.uid
               }
           });
       }
   },
   created() {
-      if(!!this.$route.params.code) {
-          this.code = this.$route.params.code
-          this.getOpenid()
-      } else {
-          this.wxAuthod()
-      }
+    if(!!this.$route.params.code) {
+        this.code = this.$route.params.code
+        this.getOpenid()
+    } else {
+        if(!!!this.GLOBAL.userProfile) {
+            this.$router.push('/mine')
+            console.log('goto route mine')
+        } else if( !!!this.GLOBAL.userProfile.wxopenid) {
+            this.wxAuthod()
+        } else {
+            this.wxopenid = this.GLOBAL.userProfile.wxopenid
+            this.uid = this.GLOBAL.userProfile.uid      
+            console.log('has wxopenid & uid ', this.wxopenid, this.uid)   
+        }
+    }
   }
 }
 </script>
