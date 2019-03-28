8f1c5ec76362a7c9e4538d0e56d3fa5f884782ef
ThomasFuzhou <29013551@qq.com>
2019-03-28 11:57:54 +0800

    fix

--- src/pages/ManyRedSend.vue
+++ src/pages/ManyRedSend.vue
@@ -1,42 +1,50 @@
 <template>
   <div>
-    <!--
-    <tab :line-width="3" custom-bar-width="60px" v-model="tabIndex">
-      <tab-item v-for="(item, index) in tabItems" :key="index" @on-item-click="onItemClick">
-        <span style="font-size:15px;font-weight:620;">{{item.label}}</span>
-      </tab-item>
-    </tab>
-    <div v-if="tabIndex==0">
-      <PropSales></PropSales>
-    </div>
-    <div v-else>
-      <Contract></Contract>
-    </div>
-    -->
-  Send
+<box gap="10px 10px">
+    <group>
+      <x-input title="总金额"  placeholder="0.000"></x-input>
+    </group>
+          
+    <group title="当前钱包剩余金额XXXX千克">
+      <x-input title="红包个数"  placeholder="请填写个数"></x-input>
+      
+    </group>
+    <group title="单个红包金额随机">
+      <x-input placeholder="祝福语：恭喜发财，万事如意！"></x-input>
+    </group>
+
+    <flexbox>
+      <flexbox-item :span="3"></flexbox-item>
+      <flexbox-item :span="6"><div align="center" style="font-size:20px;margin-top:45px;">总数10000千克</div></flexbox-item>
+    </flexbox>
+    <flexbox>
+      <flexbox-item :span="3"></flexbox-item>
+      <flexbox-item :span="6"><div align="center" style="margin-top:35px;"><x-button type="warn">塞游戏金进红包</x-button></div></flexbox-item>
+    </flexbox>
+    </box>
   </div>
 </template>
 <script>
-import { Tab, TabItem} from 'vux'
+import {Flexbox,FlexboxItem,Divider,XInput, Group, XButton, Cell,Box, } from 'vux'
 
 
-const tabList = () => [
-  {
-    label:'道具交易'
-  },{
-    label:'比特币交易'
-  }]
+
 
 export default {
   components: {
-    Tab,
-    TabItem,
-
+    Flexbox,
+    FlexboxItem,
+    Divider,
+    XInput,
+    XButton,
+    Group,
+    Cell,
+    Box,
   },
   data () {
     return {
       tabIndex: 0,
-      tabItems: tabList()
+
     }
   },
   methods: {
