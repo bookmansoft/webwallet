1db8921681238e41440ccd60ec7a2571e9053c88
ThomasFuzhou <29013551@qq.com>
2019-05-27 15:34:01 +0800

    fix

--- src/pages/Crowd.vue
+++ src/pages/Crowd.vue
@@ -9,202 +9,256 @@
       <div v-if="tabIndex==0">
         <div class="crowd-car">
           <img :src="topItem.pic" class="img-top">
-              <flexbox>
-                <flexbox-item :span="8">
-                    <div class="flex-left">
-                      <span style="font-size:17px;font-weight:590;">{{topItem.cname}}</span>
-                    </div>
-                </flexbox-item>
-                <flexbox-item :span="4">
+          <flexbox>
+            <flexbox-item :span="8">
+              <div class="flex-left">
+                <span style="font-size:17px;font-weight:590;">{{topItem.cname}}</span>
+              </div>
+            </flexbox-item>
+            <!-- <flexbox-item :span="4">
                   <div class="flex-right">
                       <p style="margin:0 auto; overflow:hidden; display:block; height:40px;line-height:40px;">
                         <img src="static/img/crowd/hot.png" style="width:18px; heigth:18px;" />
                         <span style="font-size: 15px; color:green;">众筹中</span>
                       </p>
                   </div>
-                </flexbox-item>
-              </flexbox>
+            </flexbox-item>-->
+          </flexbox>
 
-              <flexbox style="height:40px;line-height:40px;">
-                <flexbox-item :span="8">
-                    <div class="flex-left">
-                      <span style="color:coral; font-size:16px;">￥ {{topItem.price}}元起售</span>
-                    </div>
-                </flexbox-item>
-                <flexbox-item :span="4">
-                  <div class="flex-right" >
-                      <x-button mini :gradients="['#FF5E3A', '#FF9500']" @click.native="crowdDetail(topItem, 0)">立即支持</x-button>
-                  </div>
-                </flexbox-item>
-              </flexbox>
+          <flexbox style="height:40px;line-height:40px;">
+            <flexbox-item :span="8">
+              <div class="flex-left">
+                <span style="color:coral; font-size:16px;">￥ {{topItem.price}}</span>
+              </div>
+            </flexbox-item>
+            <flexbox-item :span="4">
+              <div class="flex-right">
+                <x-button
+                  mini
+                  :gradients="['#FF5E3A', '#FF9500']"
+                  @click.native="crowdDetail(topItem, 0)"
+                >立即支持</x-button>
+              </div>
+            </flexbox-item>
+          </flexbox>
 
-              <flexbox style="height:30px;line-height:30px; color:#888; font-size:14px;">
-                <flexbox-item :span="8">
-                    <div class="flex-left">
-                      <p><span>{{topItem.support}}人支持</span></p>
-                    </div>
-                </flexbox-item>
-                <flexbox-item :span="4">
-                  <div class="flex-right" >
-                      <p><span>还剩{{topItem.remainder}}个</span></p>
-                  </div>
-                </flexbox-item>
-              </flexbox>
-
+          <flexbox style="height:30px;line-height:30px; color:#888; font-size:14px;">
+            <flexbox-item :span="8">
+              <div class="flex-left">
+                <p>
+                  <span>{{topItem.support}}人支持</span>
+                </p>
+              </div>
+            </flexbox-item>
+            <flexbox-item :span="4">
+              <div class="flex-right">
+                <p>
+                  <span>还剩{{topItem.remainder}}个</span>
+                </p>
+              </div>
+            </flexbox-item>
+          </flexbox>
         </div>
 
         <div v-for="(item, index) in crowdItems" :key="index" class="crowdItem">
-              <flexbox @click.native="crowdDetail(item, index)">
-                <flexbox-item :span="2.5" style="padding:0.3rem;">
-                  <div class="flex-demo-left">
-                    <img :src="item.item_pic" class="img-game-list" />
-                  </div></flexbox-item>
-                <flexbox-item>
-                  <div style="padding-left:6px;">
-                    <p><span style="font-size:15px;">{{item.cname}}</span></p>
-                    <p><span style="color: coral; font-size:14px;">￥ {{item.price}}元起售</span></p>
-                    <p>
-                      <flexbox>
-                      <flexbox-item :span="8"><p><span style="color: #888; font-size:13px;">{{item.support}}人支持</span></p></flexbox-item>
-                      <flexbox-item :span="4"><p><span style="color: #888; font-size:13px;">剩余13天</span></p></flexbox-item>
-                      </flexbox>
-                    </p>
-                  </div>
-                </flexbox-item>
-              </flexbox>
+          <flexbox @click.native="crowdDetail(item, index)">
+            <flexbox-item :span="2.5" style="padding:0.3rem;">
+              <div class="flex-demo-left">
+                <img :src="item.item_pic" class="img-game-list">
+              </div>
+            </flexbox-item>
+            <flexbox-item>
+              <div style="padding-left:6px;">
+                <p>
+                  <span style="font-size:15px;">{{item.cname}}</span>
+                </p>
+                <p>
+                  <span style="color: coral; font-size:14px;">￥ {{item.price}}</span>
+                </p>
+                <p>
+                  <flexbox>
+                    <flexbox-item :span="8">
+                      <p>
+                        <span style="color: #888; font-size:13px;">{{item.support}}人支持</span>
+                      </p>
+                    </flexbox-item>
+                    <flexbox-item :span="4">
+                      <p>
+                        <span style="color: #888; font-size:13px;">剩余13天</span>
+                      </p>
+                    </flexbox-item>
+                  </flexbox>
+                </p>
+              </div>
+            </flexbox-item>
+          </flexbox>
         </div>
-
       </div>
 
       <div v-else>
         <div v-for="(item, index) in crowdFreeItems" :key="index" class="crowdItem2">
-              <flexbox @click.native="crowFreedDetail(item, index)">
-                <flexbox-item :span="2.5" style="padding:0.3rem;">
-                  <div class="flex-demo-left">
-                    <img :src="item.src" class="img-game-list2" />
-                  </div></flexbox-item>
-                <flexbox-item>
-                  <div style="padding-left:0px;">
-                    <p><span style="font-size:15px;">{{item.title}}</span></p>
-                    <p>
-                      <flexbox>
-                      <flexbox-item :span="8"><p><span style="color: #888; font-size:13px;">{{item.sales}}</span></p></flexbox-item>
-                      <flexbox-item :span="4"><p><span style="color: red; font-size:13px;">{{item.gold}}</span></p></flexbox-item>
-                      </flexbox>
-                    </p>
-                  </div>
-                </flexbox-item>
-              </flexbox>
+          <flexbox @click.native="crowFreedDetail(item, index)">
+            <flexbox-item :span="2" style="padding:0.1rem;">
+              <div class="flex-demo-left">
+                <img :src="item.src" class="img-game-list2">
+              </div>
+            </flexbox-item>
+            <flexbox-item :span="6">
+              <div style="padding-left:0px;">
+                <p style="height:24px">
+                  <span style="font-size:15px;">{{item.title}}</span>
+                </p>
+                <p>
+                  <flexbox>
+                    <flexbox-item :span="6">
+                      <p>
+                        <span style="color: #888; font-size:13px;">{{item.sales}}</span>
+                      </p>
+                    </flexbox-item>
+                    <flexbox-item :span="6">
+                      <p>
+                        <span style="font-size:13px;">{{item.gold}}</span>
+                      </p>
+                    </flexbox-item>
+                  </flexbox>
+                </p>
+              </div>
+            </flexbox-item>
+            <flexbox-item :span="4">
+              <div style="padding-left:0px;">
+                <p>
+                  <span v-if="item.gold>=item.baseGold" style="color: red; font-size:15px;">+ {{parseInt(item.gold*100/item.baseGold-100)}}%</span>
+                  <span v-if="item.gold<item.baseGold" style="color: blue; font-size:15px;">- {{parseInt(100-item.gold*100/item.baseGold)}}%</span>
+                </p>
+              </div>
+            </flexbox-item>>
+          </flexbox>
         </div>
       </div>
     </div>
 
     <div v-if="!isLoadMore">
-        <load-more tip="正在加载" style="position: relative; top:200px;" :show-loading="!isLoadMore"></load-more>
+      <load-more tip="正在加载" style="position: relative; top:200px;" :show-loading="!isLoadMore"></load-more>
     </div>
 
     <navs></navs>
-
   </div>
 </template>
 <script>
 //import { XHeader, Group, Cell } from 'vux'
-import { XButton, Tab, TabItem, Flexbox, FlexboxItem, LoadMore} from 'vux'
-import Navs from '@/components/Navs.vue'
+import { XButton, Tab, TabItem, Flexbox, FlexboxItem, LoadMore } from "vux";
+import Navs from "@/components/Navs.vue";
 
 const tabList = () => [
   {
-    label:'众筹大厅'
-  },{
-    label:'自由市场'
-}]
+    label: "众筹大厅"
+  },
+  {
+    label: "自由市场"
+  }
+];
 
 const crowdList = () => [
   {
-    src: 'static/img/crowd/item1.jpg',
-    title: 'Forza Horizon 3',
-    desc: '￥ 25元起售',
-    support: '60人支持',
-    remainder: '剩余13天'
-  },{
-    src: 'static/img/crowd/item2.jpg',
-    title: 'Forza Horizon 3',
-    desc: '￥ 47元起售',
-    support: '52人支持',
-    remainder: '剩余8天'
-  },{
-    src: 'static/img/crowd/item3.jpg',
-    title: 'Forza Horizon 3',
-    desc: '￥ 47元起售',
-    support: '139人支持',
-    remainder: '剩余5天'
+    src: "static/img/crowd/item1.jpg",
+    title: "Forza Horizon 3",
+    desc: "￥ 25元起售",
+    support: "60人支持",
+    remainder: "剩余13天"
+  },
+  {
+    src: "static/img/crowd/item2.jpg",
+    title: "Forza Horizon 3",
+    desc: "￥ 47元起售",
+    support: "52人支持",
+    remainder: "剩余8天"
+  },
+  {
+    src: "static/img/crowd/item3.jpg",
+    title: "Forza Horizon 3",
+    desc: "￥ 47元起售",
+    support: "139人支持",
+    remainder: "剩余5天"
   }
-]
+];
 
 const crowdFreeList = () => [
   {
-    src: 'static/img/crowd/a.jpg',
-    title: '进击的兵长 代练宝宝',
-    sales: '15个挂单出售',
-    gold: '11000.000',
-    group: 'Vallnet Co., Ltd'
-  },{
-    src: 'static/img/crowd/item1.jpg',
-    title: 'Forza Horizon 代练宝宝 ',
-    sales: '20个挂单出售',
-    gold: '800.00',
-    group: 'Vallnet Co., Ltd'
+    src: "static/img/crowd/a.jpg",
+    title: "进击的兵长 代练宝宝",
+    sales: "15个挂单出售",
+    gold: 52.222,
+    baseGold: 10,
+    group: "Vallnet Co., Ltd"
+  },
+  {
+    src: "static/img/crowd/item1.jpg",
+    title: "Forza Horizon 代练宝宝 ",
+    sales: "20个挂单出售",
+    gold: 18.000,
+    baseGold: 20,
+    group: "Vallnet Co., Ltd"
   }
-]
+];
 
 export default {
   components: {
-    Navs, Tab, XButton, TabItem, Flexbox, FlexboxItem, LoadMore
+    Navs,
+    Tab,
+    XButton,
+    TabItem,
+    Flexbox,
+    FlexboxItem,
+    LoadMore
   },
-  data () {
+  data() {
     return {
-      msg: '众筹',
+      msg: "众筹",
       tabIndex: 0,
       tabItems: tabList(),
       topItem: null,
       crowdItems: [],
       crowdFreeItems: crowdFreeList(),
       isLoadMore: false
-    }
+    };
   },
   methods: {
     onItemClick(index) {
-      console.log(this.tabIndex)
+      console.log(this.tabIndex);
     },
     crowdDetail(item, index) {
-      this.$router.push({ name: 'CrowdInfo', params: {item: item }})
+      this.$router.push({ name: "CrowdInfo", params: { item: item } });
     },
     crowFreedDetail(item, index) {
-      this.$router.push({ name: 'CrowdFreeInfo', params: { item: item }})
+      this.$router.push({ name: "CrowdFreeInfo", params: { item: item } });
     },
-    getStocks(){
-        let data = {func:'Stocks', control: 'stock', oemInfo: this.GLOBAL.oemInfo}
-        this.axios.post(this.GLOBAL.apiUrl, data).then(res => {
-            console.log('mine', res.data)
-            this.isLoadMore = true
-            if(res.data.errcode == 'success') {
-              //this.crowdItems = res.data.data
-              this.topItem = res.data.data[0]
-              let index = 0
-              res.data.data.forEach(element => {
-                if(index > 0) {
-                  this.crowdItems.push(element)
-                }
-                index++
-              });
+    getStocks() {
+      let data = {
+        func: "Stocks",
+        control: "stock",
+        oemInfo: this.GLOBAL.oemInfo
+      };
+      this.axios.post(this.GLOBAL.apiUrl, data).then(res => {
+        console.log("mine", res.data);
+        this.isLoadMore = true;
+        if (res.data.errcode == "success") {
+          //this.crowdItems = res.data.data
+          this.topItem = res.data.data[0];
+          let index = 0;
+          res.data.data.forEach(element => {
+            if (index > 0) {
+              this.crowdItems.push(element);
             }
-        });
-    },
+            index++;
+          });
+        }
+      });
+    }
   },
   created() {
-    this.getStocks()
+    this.getStocks();
   }
-}
+};
 </script>
 
 <style scoped>
@@ -231,31 +285,31 @@ export default {
 }
 
 .crowd-car {
-  padding: 10px; 
-  /*background-color: white; */ 
+  padding: 10px;
+  /*background-color: white; */
   /*border-radius: 4%;*/
 }
 
 .img-game-list {
-    width: 75px;
-    height: 75px;
-    border-radius: 12%;
-    margin-left: 3px;
+  width: 75px;
+  height: 75px;
+  border-radius: 12%;
+  margin-left: 3px;
 }
 
 .img-game-list2 {
-    width: 65px;
-    height: 65px;
-    border-radius: 10%;
-    margin-left: 4px;
+  width: 45px;
+  height: 45px;
+  border-radius: 10%;
+  margin-left: 4px;
 }
 
 .img-top {
-  width:100%;
-  height:180px;
+  width: 100%;
+  height: 180px;
   /*border-radius: 4%;*/
-  border-top-left-radius:1.5em; 
-  border-top-right-radius:1.5em; 
+  border-top-left-radius: 1.5em;
+  border-top-right-radius: 1.5em;
 }
 .imgDemo {
   width: 100%;
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <tab :line-width="3" custom-bar-width="60px" v-model="tabIndex">
+    <!-- <tab :line-width="3" custom-bar-width="60px" v-model="tabIndex">
       <tab-item v-for="(item, index) in tabItems" :key="index" @on-item-click="onItemClick">
         <span style="font-size:15px;font-weight:620;">{{item.label}}</span>
       </tab-item>
-    </tab>
+    </tab> -->
     <div v-if="tabIndex==0">
       <games></games>
     </div>
