47c6ab15474eaa03f7f8b1e7eb4837518fdb68ad
ThomasFuzhou <29013551@qq.com>
2019-06-05 23:19:38 +0800

    fix

--- src/pages/Crowd.vue
+++ src/pages/Crowd.vue
@@ -6,78 +6,83 @@
           <img src="static/img/stock/banner.png" style="width:100%">
         </box>
         <!-- todo: 替换为vue专用轮播组件 -->
+        <div style="margin-top:-10px">
+          <flexbox>
+            <flexbox-item :span="12">
+              <div class="flex-left">
+                <img src="static/img/stock/hot_line.png" style="width:3px;height:13px">
+                <span style="font-size:15px;">热门众筹</span>
+              </div>
+            </flexbox-item>
+          </flexbox>
+        </div>
+        <div style="margin-top:-30px">
+          <div class="flex-left" style="position: relative;top:47px;left:-5px">
+            <img src="static/img/stock/ren_qi.png" style="width:100px;height:27px">
+          </div>
+          <div v-for="(item, index) in crowdItems" :key="index" class="crowdItem">
+            <div class="crowd-car">
+              <img :src="item.large_img_url" class="img-top">
+              <flexbox>
+                <flexbox-item :span="12">
+                  <div class="flex-left">
+                    <span style="font-size:15px;">{{item.funding_text}}</span>
+                  </div>
+                </flexbox-item>
+              </flexbox>
 
-        <flexbox>
-          <flexbox-item :span="12">
-            <div class="flex-left">
-              <img src="static/img/stock/hot_line.png" style="width:3px;height:13px">
-              <span style="font-size:15px;">热门众筹</span>
+              <flexbox>
+                <flexbox-item :span="12">
+                  <div class="flex-left">
+                    <span style="font-size:15px;">
+                      <img src="static/img/stock/headimg.png" style="width:22px;height:22px">
+                      土豆先生
+                    </span>
+                  </div>
+                </flexbox-item>
+              </flexbox>
+
+              <flexbox>
+                <flexbox-item :span="12">
+                  <box gap="10px">
+                    <x-progress :percent="percent2" :show-cancel="false"></x-progress>
+                  </box>
+                  <!-- 无法设置颜色，需要更换实现方式 -->
+                </flexbox-item>
+              </flexbox>
+
+              <flexbox style="height:40px;line-height:40px;">
+                <flexbox-item :span="4">
+                  <div class="flex-left">
+                    <img src="static/img/stock/stock_jiner.png" style="width:15px;hegith:15px">
+                    <span style="color:coral; font-size:12px;">￥ {{item.stock_money}}</span>
+                  </div>
+                </flexbox-item>
+                <flexbox-item :span="4">
+                  <div class="flex-left">
+                    <img src="static/img/stock/stock_renshu.png" style="width:15px;hegith:15px">
+                    <span style="font-size:12px;">￥{{item.supply_people_num}}</span>
+                  </div>
+                </flexbox-item>
+                <flexbox-item :span="4">
+                  <div class="flex-left">
+                    <img src="static/img/stock/stock_shichang.png" style="width:15px;hegith:15px">
+                    <span
+                      style="font-size:12px;"
+                    >￥ {{parseInt(item.funding_done_amount*100/item.funding_target_amount) + '%'}}</span>
+                  </div>
+                </flexbox-item>
+                <flexbox-item :span="4">
+                  <div class="flex-right">
+                    <x-button
+                      mini
+                      :gradients="['#FF5E3A', '#FF9500']"
+                      @click.native="crowdDetail(item, 0)"
+                    >立即支持</x-button>
+                  </div>
+                </flexbox-item>
+              </flexbox>
             </div>
-          </flexbox-item>
-        </flexbox>
-
-        <div v-for="(item, index) in crowdItems" :key="index" class="crowdItem">
-          <div class="crowd-car">
-            <img :src="item.large_img_url" class="img-top">
-            <flexbox>
-              <flexbox-item :span="12">
-                <div class="flex-left">
-                  <span style="font-size:15px;">{{item.funding_text}}</span>
-                </div>
-              </flexbox-item>
-            </flexbox>
-
-            <flexbox>
-              <flexbox-item :span="12">
-                <div class="flex-left">
-                  <span style="font-size:15px;">
-                    <img src="static/img/stock/headimg.png" style="width:22px;height:22px">
-                    土豆先生
-                  </span>
-                </div>
-              </flexbox-item>
-            </flexbox>
-
-            <flexbox>
-              <flexbox-item :span="12">
-                <box gap="10px">
-                  <x-progress :percent="percent2" :show-cancel="false"></x-progress>
-                </box>
-                <!-- 无法设置颜色，需要更换实现方式 -->
-              </flexbox-item>
-            </flexbox>
-
-            <flexbox style="height:40px;line-height:40px;">
-              <flexbox-item :span="4">
-                <div class="flex-left">
-                  <img src="static/img/stock/stock_jiner.png" style="width:15px;hegith:15px">
-                  <span style="color:coral; font-size:12px;">￥ {{item.stock_money}}</span>
-                </div>
-              </flexbox-item>
-              <flexbox-item :span="4">
-                <div class="flex-left">
-                  <img src="static/img/stock/stock_renshu.png" style="width:15px;hegith:15px">
-                  <span style="font-size:12px;">￥{{item.supply_people_num}}</span>
-                </div>
-              </flexbox-item>
-              <flexbox-item :span="4">
-                <div class="flex-left">
-                  <img src="static/img/stock/stock_shichang.png" style="width:15px;hegith:15px">
-                  <span
-                    style="font-size:12px;"
-                  >￥ {{parseInt(item.funding_done_amount*100/item.funding_target_amount) + '%'}}</span>
-                </div>
-              </flexbox-item>
-              <flexbox-item :span="4">
-                <div class="flex-right">
-                  <x-button
-                    mini
-                    :gradients="['#FF5E3A', '#FF9500']"
-                    @click.native="crowdDetail(item, 0)"
-                  >立即支持</x-button>
-                </div>
-              </flexbox-item>
-            </flexbox>
           </div>
         </div>
       </div>
