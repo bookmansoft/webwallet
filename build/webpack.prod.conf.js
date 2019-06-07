a56e7f1d5002056add5189e113b776fbb3ee39b1
bookman <ceo@920.cc>
2019-06-07 15:10:32 +0800

    升级了若干引发安全性报警的依赖包 去除了如下不必要的依赖： OptimizeCSSPlugin co-wechat-api

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,7 +8,6 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
