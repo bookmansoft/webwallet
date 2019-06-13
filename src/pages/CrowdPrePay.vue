85ac13fff6793b4a5ac1559ccb2f34da88c5698e
ThomasFuzhou <29013551@qq.com>
2019-06-13 18:02:56 +0800

    fix

--- src/pages/CrowdPrePay.vue
+++ src/pages/CrowdPrePay.vue
@@ -1,13 +1,63 @@
 // 选择回报，及支付的前一页
 <template>
   <div class="root">
-    <flexbox>
-      <flexbox-item :span="12">
-        <div style="height:6px;background-color:#f3f3f3"></div>
-      </flexbox-item>
-    </flexbox>
-
-    
+    <div style="background-color:#f3f3f3">
+      <flexbox>
+        <flexbox-item :span="12">
+          <div style="height:40px;"></div>
+        </flexbox-item>
+      </flexbox>
+      <flexbox>
+        <flexbox-item :span="12">
+          <div style="margin-left:25px;height:40px;">
+            <span
+              style="font-size:15px;font-family:'黑体','Heiti SC','Droidsansfallback';font-weight:bold;color:rgb(50,58,69);"
+            >风险提示：</span>
+          </div>
+        </flexbox-item>
+      </flexbox>
+      <flexbox>
+        <flexbox-item :span="12">
+          <div style="margin-left:25px;">
+            <span
+              style="font-size:14px;font-family:'黑体','Heiti SC','Droidsansfallback';font-weight:bold;color:rgb(50,58,69);"
+            >1、您参与众筹是支持创意变为现实的过程，而不是直接的商品交易，因此存在一定风险。请您根据自己的判断选择，支持众筹项目。</span>
+          </div>
+        </flexbox-item>
+      </flexbox>
+      <flexbox style="margin-top:45px" v-if="flagMore==false">
+        <flexbox-item :span="12">
+          <div style="height:40px;text-align:center;" @click="viewMore">
+            <span
+              style="font-size:15px;font-family:'黑体','Heiti SC','Droidsansfallback';font-weight:bold;color:rgb(50,58,69);"
+            >查看更多▼</span>
+          </div>
+        </flexbox-item>
+      </flexbox>
+      <flexbox style="margin-top:14px;" v-if="flagMore==true">
+        <flexbox-item :span="12">
+          <div style="display:block;margin-left:21px" >
+            <span
+              style="font-size:14px;font-family:'黑体','Heiti SC','Droidsansfallback';font-weight:bold;color:rgb(50,58,69);"
+            >2、众筹商户与本平台均不承诺任何形式的收益。</span>
+          </div>
+        </flexbox-item>
+      </flexbox>
+      <flexbox style="margin-top:14px;" v-if="flagMore==true">
+        <flexbox-item :span="12">
+          <div style="display:block;margin-left:21px" >
+            <span
+              style="font-size:14px;font-family:'黑体','Heiti SC','Droidsansfallback';font-weight:bold;color:rgb(50,58,69);"
+            >3、请不要听信他人的建议。</span>
+          </div>
+        </flexbox-item>
+      </flexbox>
+      <flexbox>
+        <flexbox-item :span="12">
+          <div style="height:40px;"></div>
+        </flexbox-item>
+      </flexbox>
+    </div>
   </div>
 </template>
 <script>
@@ -27,15 +77,18 @@ export default {
   data() {
     return {
       item: {},
-
+      flagMore: false
     };
   },
   methods: {
+    viewMore() {
+      console.log("进入viewMore方法");
+      this.flagMore=true;
 
+    }
   },
   created() {
     this.item = this.$route.params.item;
-
   }
 };
 </script>
