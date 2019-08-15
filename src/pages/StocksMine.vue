8b668b4175ce2f26c9fb2c9d9afe0771374d4077
bookman <ceo@920.cc>
2019-08-15 16:26:04 +0800

    凭证列表增加返回链接

--- src/pages/StocksMine.vue
+++ src/pages/StocksMine.vue
@@ -2,7 +2,7 @@
 -->
 <template>
   <div>
-    <!-- <x-header :left-options="{preventGoBack: true}" @on-click-back="onBack">{{headerTitle}}</x-header> -->
+    <x-header :left-options="{preventGoBack: true}" @on-click-back="onBack">{{headerTitle}}</x-header>
     <MyStock :showType="1"></MyStock>
     <!-- 导航 -->
     <navs></navs>
