84838037906eb481d1747b85a06b6d989d6abfad
bookman <ceo@920.cc>
2019-10-02 21:57:28 +0800

    change namespace

--- src/pages/TransList.vue
+++ src/pages/TransList.vue
@@ -171,11 +171,11 @@ import {
   CellFormPreview,
   Badge
 } from "vux";
-import Navs from "@/components/Navs.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
   components: {
-    Navs,
+    Nav,
     Tab,
     XButton,
     XHeader,
