3823e431de8764718d0c7b18d0f980992bab91cf
bookman <ceo@920.cc>
2019-10-02 21:57:10 +0800

    change namespace

--- src/pages/SendFail.vue
+++ src/pages/SendFail.vue
@@ -72,11 +72,11 @@ import {
   CellFormPreview,
   Badge
 } from "vux";
-import Navs from "@/components/Navs.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
   components: {
-    Navs,
+    Nav,
     Tab,
     XButton,
     XHeader,
